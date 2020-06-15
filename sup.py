ef69a88fad99dc80175df57c71007fe6a8bd59f7
datashaman <marlinf@datashaman.com>
2020-06-15 11:10:29 +0200

    fixes

--- sup.py
+++ sup.py
@@ -3,7 +3,6 @@ import frontmatter
 import parsedatetime
 import re
 import os
-import uuid
 
 from datetime import datetime
 from github import Github
@@ -16,7 +15,7 @@ cal = parsedatetime.Calendar()
 TZ = os.getenv('TZ')
 
 def create_post(entry):
-    pattern = re.compile(r'^((?P<date>[^:]*):)?\s*(?P<body>.*)$', re.MULTILINE)
+    pattern = re.compile(r'^((?P<date>[^:]*):\s+)?\s*(?P<body>.*)$', re.MULTILINE)
     match = pattern.match(entry)
     metadata = match.groupdict()
 
@@ -39,11 +38,20 @@ def cli(entry):
     post = create_post(' '.join(entry))
 
     g = Github(os.getenv('GITHUB_TOKEN'))
+    repo = g.get_user().get_repo(os.getenv('GITHUB_REPO'))
 
     posts = os.getenv('GITHUB_POSTS', '_posts')
-    published = post['date'].strftime('%Y-%m-%d')
-    post_id = uuid.uuid4()
-    path = f'{posts}/{published}-{post_id}.md'
+    filename = post['date'].strftime('%Y-%m-%d-%H%M%S')
+    path = f'{posts}/{filename}.md'
     message = 'sup'
     content = frontmatter.dumps(post)
-    g.get_user().get_repo(os.getenv('GITHUB_REPO')).create_file(path, message, content)
+    branch = os.getenv('GITHUB_BRANCH')
+
+    response = repo.create_file(
+        path,
+        message,
+        content,
+        branch=branch
+    )
+
+    click.echo(response['content'].html_url)
