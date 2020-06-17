fa2fdce9d740779abaadd1421221a670c2250fd6
datashaman <marlinf@datashaman.com>
2020-06-17 08:46:54 +0200

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,13 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    /*
     ...(process.env.JEKYLL_ENV == 'production'
       ? [
           require('cssnano')({ preset: 'default' }),
         ]
       : []
     ),
+    */
   ],
 }
